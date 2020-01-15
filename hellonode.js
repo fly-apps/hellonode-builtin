aae078e5e94adb5b00a5e3c3cb56c4e5531f3613
Dj Walker-Morgan <codepope@gmail.com>
2020-01-15 18:23:43 +0000

    Move to prefix the 'hello' name with /to/

--- hellonode.js
+++ hellonode.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get(['/', '/:name'], (req, res) => {
+app.get(['/', '/to/:name'], (req, res) => {
     greeting = '<h1>Hello From Node on Fly!</h1>'
     name = req.params["name"]
     if (name) {
