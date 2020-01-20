52d7d3efd6b9b06b5c249266426ebbeb1a90e93a
Dj Walker-Morgan <codepope@gmail.com>
2020-01-20 10:40:15 +0000

    Prettier Formatted

--- hellonode.js
+++ hellonode.js
@@ -1,16 +1,15 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get(['/', '/to/:name'], (req, res) => {
-    greeting = '<h1>Hello From Node on Fly!</h1>'
-    name = req.params["name"]
-    if (name) {
-        res.send(greeting + '</br>and hello to ' + name)
-    } else {
-        res.send(greeting)
-    }
-}
-)
+app.get(["/", "/to/:name"], (req, res) => {
+  greeting = "<h1>Hello From Node on Fly!</h1>";
+  name = req.params["name"];
+  if (name) {
+    res.send(greeting + "</br>and hello to " + name);
+  } else {
+    res.send(greeting);
+  }
+});
 
-app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`))
+app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
