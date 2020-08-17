e92c078e59e3264a00fa1b962a8bface96b034a8
Dj Walker-Morgan <codepope@gmail.com>
2020-08-17 14:53:05 +0100

    138 updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# hellonode
-
-## A Fly Example
-
-This is a simple application used in the [fly.io]() Getting Started documentation showing how to create a Dockerfile for a Node application and use that to deploy to the Fly Global Application Platform.
-
-
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get(["/", "/to/:name"], (req, res) => {
+app.get(["/", "/:name"], (req, res) => {
   greeting = "<h1>Hello From Node on Fly!</h1>";
   name = req.params["name"];
   if (name) {
@@ -12,4 +12,5 @@ app.get(["/", "/to/:name"], (req, res) => {
   }
 });
 
-app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
+app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`))
+
