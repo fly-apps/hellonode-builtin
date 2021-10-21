119cbd2fb8bfc0c2977683d74a0e973a79ac3002
Vishal Lama <vishal.lama@gmail.com>
2021-10-21 16:42:42 -0600

    [refactor] Declare variables as `const`
    Declare the variable `greeting` as const, as recommended for variables
    that are constants. Also, declare the variable `name` as const, since
    this is considered better style, especially since the global `name`
    variable has special meaning.

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get(["/", "/:name"], (req, res) => {
-  greeting = "<h1>Hello From Node on Fly!</h1>";
-  name = req.params["name"];
+  const greeting = "<h1>Hello From Node on Fly!</h1>";
+  const name = req.params["name"];
   if (name) {
     res.send(greeting + "</br>and hello to " + name);
   } else {
